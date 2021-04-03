cf634da6ff0b979848ef906ef324ab978f04e538
quanghuuxx <quanghuuxx@gmail.com>
2021-04-03 14:27:29 +0700

    new

--- lib/controllers/controller_imagereading.dart
+++ lib/controllers/controller_imagereading.dart
@@ -1,14 +1,13 @@
 import 'package:get/get.dart';
 
 class ControllerReadImage extends GetxController {
-  List<String> imageLink = List<String>().obs;
-  List<bool> imageisChose = List<bool>().obs;
-
-  List<String> imageChosenLink = List<String>().obs;
+  List<String> imageLink = <String>[].obs;
+  List<bool> imageisChose = <bool>[].obs;
+  List<String> imageChosenLink = <String>[].obs;
   int limit = 21;
   int offset = 0;
   RxBool isLoad = false.obs;
-
+  String a;
   int maxLenght = 0;
   bool isFull = false;
 }
